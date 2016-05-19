714a225088d334b890e75c9a1f8a442bd9d53c3f
David Wong <neoplastic@gmail.com>
2016-05-19 10:08:32 +1000

    Initial commit

---
+++ Example/RxFirebase/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RxFirebase
+//
+//  Created by David Wong on 05/19/2016.
+//  Copyright (c) 2016 David Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
