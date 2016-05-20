7cfa06487d5c58e973a58a7b99025213f954e8b2
David Wong <neoplastic@gmail.com>
2016-05-20 10:48:07 +1000

    Documented RxFirebaseAuth

--- RxFirebase/Classes/RxFirebaseAuth.swift
+++ RxFirebase/Classes/RxFirebaseAuth.swift
@@ -11,6 +11,12 @@ import FirebaseAuth
 import RxSwift
 
 public extension FIRAuth {
+    /**
+     Registers for an "auth state did change" observable. Invoked when:
+     - Registered as a listener
+     - The current user changes, or,
+     - The current user's access token changes.
+     */
     var rx_addAuthStateDidChangeListener: Observable<(FIRAuth, FIRUser?)> {
         get {
             return Observable.create { observer in
@@ -25,6 +31,11 @@ public extension FIRAuth {
         }
     }
     
+    /**
+     Sign in with email address and password.
+     @param email The user's email address.
+     @param password The user's password.
+    */
     func rx_signinWithEmail(email: String, password: String) -> Observable<FIRUser?> {
         return Observable.create { observer in
             
@@ -41,6 +52,9 @@ public extension FIRAuth {
         }
     }
     
+    /** 
+        sign in anonymously
+    */
     func rx_signInAnonymously() -> Observable<FIRUser?> {
         return Observable.create { observer in
             self.signInAnonymouslyWithCompletion({ (user, error) in
@@ -56,6 +70,10 @@ public extension FIRAuth {
         }
     }
     
+    /**
+     Sign in with credential.
+     @param credentials An instance of FIRAuthCredential (Facebook, Twitter, Github, Google)
+    */
     func rx_signInWithCredentials(credentials: FIRAuthCredential) -> Observable<FIRUser?> {
         return Observable.create { observer in
             FIRAuth.auth()?.signInWithCredential(credentials, completion: { (user, error) in
@@ -71,6 +89,10 @@ public extension FIRAuth {
         }
     }
     
+    /**
+     Sign in with custom token.
+     @param A custom token. Please see Firebase's documentation on how to set this up.
+    */
     func rx_signInWithCustomToken(token: String) -> Observable<FIRUser?> {
         return Observable.create { observer in
             self.signInWithCustomToken(token, completion: { (user, error) in
@@ -86,6 +108,11 @@ public extension FIRAuth {
         }
     }
     
+    /**
+     Create and on success sign in a user with the given email address and password.
+     @param email The user's email address.
+     @param password The user's desired password
+    */
     func rx_createUserWithEmail(email: String, password: String) -> Observable<FIRUser?> {
         return Observable.create { observer in
             self.createUserWithEmail(email, password: password, completion: { (user, error) in
